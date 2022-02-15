01693633d7f1e1c8b0d30a8165e20e23097473ad
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-15 10:11:10 -0500

    Create lab_12-1.py

---
+++ lab_12-1.py
@@ -0,0 +1,8 @@
+# Author: JD 02/05/2022
+
+my_file = open("my_file.txt","w")
+
+my_file.write("JD 021522\n")
+my_file.write("Hello world\n")
+my_file.write("Playing soccer and computer games.")
+my_file.close()
