197d241565ef6444f7f2932ae643dddf4dfe7f5c
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-15 10:11:40 -0500

    Create lab_12-2.py

---
+++ lab_12-2.py
@@ -0,0 +1,9 @@
+# Author: JD 02/15/2022
+
+my_file = open("my_file.txt","a")
+
+my_file.write("\nHaving vedio calls with family members.\n")
+
+my_file.write("Go back home to see my family.")
+
+my_file.close()
