a476babc04add83f24ddc90b3671e7f40ec9a385
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-23 10:13:20 -0500

    Update lab_12-2.py

--- lab_12-2.py
+++ lab_12-2.py
@@ -1,9 +1,9 @@
 # Author: JD 02/15/2022
-
+# Open the file with append mathod.
 my_file = open("my_file.txt","a")
-
+# Adding the lines at the end of the file, starts with an escape character to start with a new line.
 my_file.write("\nHaving vedio calls with family members.\n")
 
 my_file.write("Go back home to see my family.")
-
+# Closing the file in the end.
 my_file.close()
