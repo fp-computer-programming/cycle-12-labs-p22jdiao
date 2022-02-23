a0dcee011cc2b368853ad6a5e0a0200a45f1ba2c
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-23 10:11:10 -0500

    Update lab_12-1.py

--- lab_12-1.py
+++ lab_12-1.py
@@ -1,7 +1,7 @@
 # Author: JD 02/05/2022
 # By using a open() statement, the program will open the file with writing mode.
 my_file = open("my_file.txt","w")
-# By using a .write() mathod, the program will write the contends needed with a \n in the end of each line to go to the next line.
+# By using a .write() mathod, the program will write the contends needed with an escape character '\n' in the end of each line to go to the next line.
 my_file.write("JD 021522\n")
 my_file.write("Hello world\n")
 my_file.write("Playing soccer and computer games.")
